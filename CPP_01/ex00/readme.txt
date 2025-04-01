6810e3d44b20f4b02de72c55e87901107ce34ef3
Adele Martin <adelejulietmartin@gmail.com>
2025-04-01 16:29:40 +0200

    ok

---
+++ CPP_01/ex00/readme.txt
@@ -0,0 +1,2 @@
+You will have to implement a lot of classes. This can seem tedious,
+unless you’re able to script your favorite text editor.
---
+++ CPP_01/ex00/zombie.cpp
@@ -0,0 +1,15 @@
+#ifndef CONTACT_H
+# define CONTACT_H
+#include <string>
+
+class Zombie
+{
+	private:
+	std::string name;
+
+	public:
+	void announce( void );
+	Zombie();
+};
+
+#endif
