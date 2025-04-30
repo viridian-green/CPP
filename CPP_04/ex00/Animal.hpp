b27f8278dd1b45e967e27afdb89a7b13dde39816
Adele Martin <adelejulietmartin@gmail.com>
2025-04-30 17:31:53 +0200

    ok

--- CPP_04/ex00/Animal.hpp
+++ CPP_04/ex00/Animal.hpp
@@ -14,7 +14,7 @@ class Animal
     Animal& operator=(const Animal &other);
     std::string getType() const;
     virtual void makeSound() const;
-    ~Animal();
+    virtual ~Animal();
 
 };
 
--- CPP_04/ex00/main.cpp
+++ CPP_04/ex00/main.cpp
@@ -9,9 +9,12 @@ const Animal* j = new Dog();
 const Animal* i = new Cat();
 std::cout << j->getType() << " " << std::endl;
 std::cout << i->getType() << " " << std::endl;
-i->makeSound(); 
+i->makeSound();
 j->makeSound();
 meta->makeSound();
 
+delete meta;
+delete i;
+delete j;
 return 0;
 }
