03c3971220188f7c497ef5137f514cdf1f16b577
Adèle Martin <your.email@example.com>
2025-06-30 12:08:42 +0200

    ok

--- CPP_07/ex01/incl/iter.hpp
+++ CPP_07/ex01/incl/iter.hpp
@@ -4,7 +4,7 @@
 #include <string>
 
 template <typename iter>
-void iter(int *A, size_t arraylen, f())
+void iter(int *A, size_t arraylen, std::function<int(int, int)> func) //understand how to use this
 {
 	for (int i = 0; i < arraylen; ++i)
 	{
--- CPP_07/ex01/src/main.cpp
+++ CPP_07/ex01/src/main.cpp
@@ -3,8 +3,7 @@
 int main()
 {
 // int array[4] = {1, 2, 3, 4};
-std::string = "hello";
-
+std::string s1 = "hello";
 
 
 iter(&string_array, 3, std::toupper);
