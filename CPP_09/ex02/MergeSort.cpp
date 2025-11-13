2417918627bd4a2fbbf508745ca2627290a6d6e8
Adele Martin <adelejulietmartin@gmail.com>
2025-11-13 18:48:46 +0100

    sync

--- CPP_09/ex02/MergeSort.cpp
+++ CPP_09/ex02/MergeSort.cpp
@@ -8,17 +8,6 @@
 // MergeSort::~MergeSort(){}
 
 
-//For debug purposes
-std::ostream& operator<<(std::ostream& os, const Pair& p) {
-    os << "(" << p.a << ", " << p.b << ")";
-    return os;
-}
-
-template <typename Container>
-Container MergeSort<Container>::getResult()
-{
-	return m_result;
-}
 
 void insertInSortedOrder(std::vector<int>& vec, int value) {
     auto pos = std::lower_bound(vec.begin(), vec.end(), value);
--- CPP_09/ex02/MergeSort.hpp
+++ CPP_09/ex02/MergeSort.hpp
@@ -8,11 +8,16 @@
 #include <deque>
 #include <set>
 
-//I keep the original pair and will never break that relationship
-struct Pair {
-    int a; //bigger number: on the left of pair
-    int b; //smaller number: on the right of pair
-    // Pair(int x, int y) : a(x), b(y) {}
+// //I keep the original pair and will never break that relationship
+// struct Pair {
+//     int a; //bigger number: on the left of pair
+//     int b; //smaller number: on the right of pair
+//     // Pair(int x, int y) : a(x), b(y) {}
+// };
+
+struct FirstPair {
+    int main_val;   // bigger number
+    size_t pend_idx; // index of corresponding pend element
 };
 
 template <typename Container>
@@ -30,7 +35,10 @@ class MergeSort{
 	void sort();
 	void FJalgo();
 	void printResult();
-    void mergeSort(Container& input, Container& main_chain,  Container& pend_chain);
+
+void mergeSort(Container& input,
+                                     Container& main_chain,
+                                     Container& pend_chain, std::vector<FirstPair>* first_pairs = nullptr);
     void binarySearch();
 	Container getResult();
     Container getInput();
@@ -39,9 +47,11 @@ class MergeSort{
     private:
     Container m_sequence;
 	Container m_result;
-    std::vector<Pair> pairs;
+    // std::vector<Pair> pairs;
+    std::vector<size_t> partner_indices;
     // Container::<std::Pair<int, int>> pairs;
     int form_pairs = -1;
+    // std::vector<FirstPair> *first_pairs = nullptr;
 
 };
 
@@ -74,84 +84,143 @@ Container MergeSort<Container>::jacobsthalOrder(size_t n) {
     return order;
 };
 
+// template <typename Container>
+// void MergeSort<Container>::mergeSort(Container& input, Container& main_chain,  Container& pend_chain)
+// {
+
+//     auto it = input.begin();
+//     while (it != input.end()) {
+//         auto first = *it;
+//         ++it;
+//           if (it == input.end()) {
+//             pend_chain.push_back(first);
+//             break;
+//         }
+//         if (input.size() == 3)
+//         {
+//             pend_chain.push_back(input.back());
+//             input.pop_back();
+//         }
+//         auto second = *it;
+//         ++it;
+//         if (first < second)
+//             std::swap(first, second);
+//         if (input.size() <= 2) {
+//             main_chain.push_back(second);
+//             main_chain.push_back(first);
+//             return;
+//         }
+//         main_chain.push_back(first);
+//         pend_chain.push_back(second);
+//         partner_indices.push_back(i);
+
+//     }
+//     Container new_main, new_pend;
+//     // form_pairs = 1;
+//     mergeSort(main_chain, new_main, new_pend);
+//     main_chain = new_main;
+
+//     pend_chain.insert(pend_chain.end(), new_pend.begin(), new_pend.end());
+// }
+
+// template <typename Container>
+// void MergeSort<Container>::FJalgo() {
+
+//     if (m_sequence.size() == 1)
+//         return;
+
+//     Container main_chain;
+//     Container pend_chain;
+//     mergeSort(m_sequence, main_chain, pend_chain);
+
+
+//     Container order = jacobsthalOrder(pend_chain.size());
+//       for (typename Container::value_type x : main_chain) {
+//         std::cout << x << ". ";
+//     } std::cout << "\n";
+
+//          for (typename Container::value_type x : pend_chain) {
+//         std::cout << x << "< ";
+//     } std::cout << "\n";
+
+//     for (size_t idx : order) {
+//         int val = pend_chain[idx];
+//         std::cout << val << std::endl;
+//         // find its corresponding main partner
+//         int partner = partner_indices[idx].a;
+//         std::cout << partner << std::endl;
+//         // binary search upper bound for where to insert val
+//         auto pos = std::upper_bound(main_chain.begin(), main_chain.end(), partner);
+//         main_chain.insert(pos, val);
+//     }
+
+//     std::cout << "After: ";
+// 	for (int x : main_chain) {
+// 		std::cout << x << " ";
+// 	}
+
+//     m_result = main_chain;
+// }
+
 template <typename Container>
-void MergeSort<Container>::mergeSort(Container& input, Container& main_chain,  Container& pend_chain)
+void MergeSort<Container>::mergeSort(Container& input,
+                                     Container& main_chain,
+                                     Container& pend_chain, std::vector<FirstPair>* first_pairs)
+
 {
     auto it = input.begin();
+    size_t pair_idx = 0;
+
     while (it != input.end()) {
-        auto first = *it;
-        ++it;
-          if (it == input.end()) {
-            pend_chain.push_back(first);
-            break;
-        }
-        if (input.size() == 3)
-        {
-            pend_chain.push_back(input.back());
-            input.pop_back();
-        }
-        auto second = *it;
-        ++it;
-        if (first < second)
-            std::swap(first, second);
-        if (input.size() <= 2) {
-            main_chain.push_back(second);
-            main_chain.push_back(first);
-            return;
-        }
+        auto first = *it++;
+        // if (it == input.end()) {
+        //     pend_chain.push_back(first);
+        //     if (first_pairs) first_pairs->push_back({first, pair_idx-1});
+        //     break;
+        // }
+
+        auto second = *it++;
+        if (first < second) std::swap(first, second);
+
         main_chain.push_back(first);
         pend_chain.push_back(second);
-        // if (!form_pairs)
-        // {
-            // Pair p;
-            // p.a = first;
-            // p.b = second;
-            // pairs.push_back(p);
-        // }
-    }
-    Container new_main, new_pend;
-    form_pairs = 1;
-    mergeSort(main_chain, new_main, new_pend);
-    main_chain = new_main;
 
-    pend_chain.insert(pend_chain.end(), new_pend.begin(), new_pend.end());
+        if (first_pairs) {
+            first_pairs->push_back({first, pair_idx});
+        }
+        pair_idx++;
+    }
+
+    // Recurse on main_chain
+    if (main_chain.size() > 2) {
+        Container new_main, new_pend;
+        mergeSort(main_chain, new_main, new_pend);
+        main_chain = new_main;
+        pend_chain.insert(pend_chain.end(), new_pend.begin(), new_pend.end());
+    }
 }
 
 template <typename Container>
-void MergeSort<Container>::FJalgo() {
+void MergeSort<Container>::FJalgo()
+{
+    if (m_sequence.size() <= 1) return;
 
-    if (m_sequence.size() == 1)
-        return;
+    Container main_chain, pend_chain;
 
-    Container main_chain;
-    Container pend_chain;
-    mergeSort(m_sequence, main_chain, pend_chain);
+    std::vector<FirstPair> first_pairs = nullptr;
 
+    mergeSort(m_sequence, main_chain, pend_chain,  &first_pairs);
 
     Container order = jacobsthalOrder(pend_chain.size());
-      for (typename Container::value_type x : main_chain) {
-        std::cout << x << ". ";
-    } std::cout << "\n";
-
-         for (typename Container::value_type x : pend_chain) {
-        std::cout << x << "< ";
-    } std::cout << "\n";
 
     for (size_t idx : order) {
         int val = pend_chain[idx];
-        // std::cout << val << std::endl;
-        // find its corresponding main partner
-        // int partner = pairs[idx].a;
-        // std::cout << partner << std::endl;
-        // binary search upper bound for where to insert val
+        // find original main partner from first_pairs
+        // int partner = first_pairs[idx].main_val;
+
         // auto pos = std::upper_bound(main_chain.begin(), main_chain.end(), partner);
         // main_chain.insert(pos, val);
     }
-
-    std::cout << "After: ";
-	for (int x : main_chain) {
-		std::cout << x << " ";
-	}
 
     m_result = main_chain;
 }
@@ -198,25 +267,11 @@ int MergeSort<Container>::parseInput(int ac, char **arg) {
 	return 0;
 };
 
-
-template <typename Container, typename T>
-typename Container::const_iterator binarySearch(const Container& container, const T& value)
-{
-    auto left = container.begin();
-    auto right = container.end();
-
-    while (left < right) {
-        // find the middle iterator
-        auto mid = left;
-        std::advance(mid, std::distance(left, right) / 2);
-
-        if (*mid == value)
-            return mid; // found
-        else if (*mid < value)
-            left = std::next(mid); // search right half
-        else
-            right = mid;           // search left half
-    }
-
-    return container.end(); // not found
-}
+//Pairs code
+      // if (!form_pairs)
+        // {
+            // Pair p;
+            // p.a = first;
+            // p.b = second;
+            // pairs.push_back(p);
+        // }
